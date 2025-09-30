f6816680115adca8afaf887716936106d51117eb
Leo Jensen <scouten0621@outlook.com>
2025-09-30 11:20:18 +0200

    Added shortcut

--- LiarsDice(TCP)/TCPserver/obj/Debug/net9.0-windows/TCPserver.AssemblyInfo.cs
+++ LiarsDice(TCP)/TCPserver/obj/Debug/net9.0-windows/TCPserver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TCPserver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4774555ab5843d94002a1534927451249c220d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2c4981cdba41f80ae9c73974442a2e54031508")]
 [assembly: System.Reflection.AssemblyProductAttribute("TCPserver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TCPserver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
